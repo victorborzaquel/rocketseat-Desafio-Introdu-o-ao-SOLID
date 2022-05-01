722d9df6fa4f1c3ddfb4750c98b28376e411258a
victorborzaquel <victorborzaquel@outlook.com>
2022-04-30 23:46:08 -0300

    Complete

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    const user = typeof user_id === "string" ? user_id : user_id[0];
+
+    try {
+      const userList = this.listAllUsersUseCase.execute({ user_id: user });
+
+      return response.json(userList);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User already exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("User is not a admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
